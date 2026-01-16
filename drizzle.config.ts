cbdcc822868f43babcbdc1005396b8c459af1875
Lisa <lisavansteijn@icloud.com>
2026-01-16 19:43:42 +0100

    Added database setup

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+
+import env from "./lib/env.ts";
+import "dotenv/config";
+
+export default defineConfig({
+  out: "./lib/db/migrations",
+  schema: "./lib/db/schema/index.ts",
+  casing: "snake_case",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: env.DB_FILE_NAME!,
+  },
+});
