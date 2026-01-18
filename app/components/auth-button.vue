b3a8af872c7d3d9a46bcead2ed497ae21909f677
lisavansteijn <35180144+lisavansteijn@users.noreply.github.com>
2026-01-18 14:16:24 +0100

    Added better Sign in/out functionality (#7)
    * Added better Sign in/out functionality
    
    * updated build lint for turso db

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -3,7 +3,30 @@ const authStore = useAuthStore();
 </script>
 
 <template>
+  <div v-if="authStore.loading !== true && authStore.user" class="dropdown dropdown-end dropdown-hover">
+    <div
+      tabindex="0"
+      role="button"
+      class="m-1"
+    >
+      {{ authStore.user.name }}
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-10 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+    </div>
+    <ul tabindex="-1" class="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/dashboard/sign-out">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
   <button
+    v-else
     class="btn btn-primary"
     :disabled="authStore.loading"
     @click="authStore.signIn"
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,9 +7,8 @@ export default defineConfig({
   out: "./lib/db/migrations",
   schema: "./lib/db/schema/index.ts",
   dialect: "turso",
-  casing: "snake_case",
   dbCredentials: {
-    url: env.TURSO_DATABASE_URL,
+    url: env.NODE_ENV === "development" ? "http://127.0.0.1:8080" : env.TURSO_DATABASE_URL,
     authToken: env.NODE_ENV === "development" ? undefined : env.TURSO_AUTH_TOKEN,
   },
 });
